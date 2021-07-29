09c1007b666314194350033b5a1c8c4b0c2619c9
Maksim Ksenzhuk <maksim.ksenzhuk@gmail.com>
2021-07-29 06:24:33 +0300

    edit address module

--- src/test/java/AddressModule.java
+++ src/test/java/AddressModule.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import java.io.File;
 
 class AddressModule {
-    //action with address
+    //action with address (filling the forms)
     static void addressCreating(WebDriver driver) {
         driver.findElement(By.xpath("//a[@href=\"/addresses\"]")).click();
         driver.findElement(By.xpath("//a[@data-test=\"create\"]")).click();
@@ -23,6 +23,7 @@ class AddressModule {
         driver.findElement(By.id("address_age")).sendKeys("28");
         driver.findElement(By.xpath("//input[@id=\"address_website\"]")).sendKeys("http://www.random.com");
 
+        //path to file which will be upload
         String pathToFile;
         File filePath = new File("src\\main\\resources\\Test_pict.png");
         pathToFile = filePath.getAbsolutePath();
@@ -34,5 +35,26 @@ class AddressModule {
         driver.findElement(By.xpath("//textarea[@id=\"address_note\"]")).sendKeys("test message");
         driver.findElement(By.xpath("//input[@class=\"btn btn-primary\"]")).click();
         driver.findElement(By.xpath("//a[@data-test=\"list\"]")).click();
+    }
+
+    //editing exist address
+    static void addressEditing (WebDriver driver) {
+        driver.findElement(By.xpath("(//a[starts-with(@data-test,\"edit-\")])[1]")).click();
+        driver.findElement(By.id("address_first_name")).sendKeys("1");
+        driver.findElement(By.id("address_last_name")).sendKeys("2");
+        driver.findElement(By.xpath("//input[@value=\"Update Address\"]")).click();
+        driver.findElement(By.xpath("//a[@data-test=\"list\"]")).click();
+    }
+
+    //deleting first address in list
+    static void addressDeleting (WebDriver driver) {
+        driver.findElement(By.xpath("(//a[starts-with(@data-test,\"destroy-\")])[1]")).click();
+        driver.switchTo().alert().accept();
+    }
+
+    //sign-out from address-book for current user
+    static void sighOut(WebDriver driver) {
+        driver.findElement(By.xpath("//a[@data-test=\"sign-out\"]")).click();
+        driver.quit();
     }
 }
--- src/test/java/RunnerForFirefox.java
+++ src/test/java/RunnerForFirefox.java
@@ -1,20 +1,28 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
 import java.time.Duration;
 
 public class RunnerForFirefox {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver\\geckodriver.exe");
+        //main class for running test in FF
+
+        String pathToGeckoDriver;
+        File file = new File("src\\main\\resources\\geckodriver.exe");
+        pathToGeckoDriver = file.getAbsolutePath();
+
+        System.setProperty("webdriver.gecko.driver", pathToGeckoDriver);
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.get("http://a.testaddressbook.com/");
 
         LogInModule.logIn(driver);
         AddressModule.addressCreating(driver);
-        AddressEditModule.addressEditing(driver);
-        DelAndLogout.addressDeleting (driver);
+        AddressModule.addressEditing(driver);
+        AddressModule.addressDeleting (driver);
+        AddressModule.sighOut(driver);
 
     }
 }
